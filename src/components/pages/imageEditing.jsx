a9f8a6ebc435fc395f557d8cb4852437e7770d86
AodhSully <aodhsully@gmail.com>
2021-02-02 13:46:54 +0000

    minor changes
    added photoshop work that needs to be added into site

--- src/components/pages/imageEditing.jsx
+++ src/components/pages/imageEditing.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import eva from '../../images/imageEditing/Artboard 2.png'
 import pigalle from '../../images/imageEditing/Pigalle 3.jpg'
+// import index from '../../images/imageEditing/index.jpg'
+// import index2 from '../../images/imageEditing/index2.jpg'
+// import index3 from '../../images/imageEditing/index3.jpg'
+// import index4 from '../../images/imageEditing/index4.jpg'
 
 export default class imageEditing extends React.Component {
   render() {
@@ -13,6 +17,12 @@ export default class imageEditing extends React.Component {
           <div className="imageRow">
             <img className="imageEditImages" src={eva} alt={eva} />
             <img className="imageEditImages" src={pigalle} alt={pigalle} />
+            {/*
+              <img className="imageEditImages" src={index} alt={index} />
+              <img className="imageEditImages" src={index3} alt={index3} />
+              <img className="imageEditImages" src={index4} alt={index4} />
+              <img className="imageEditImages" src={index2} alt={index2} />
+              */}
           </div>
         </div>
 
